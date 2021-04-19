372d180ed3971a818b18c33669c631767acef6b1
Wanjun Zhong <zhongwj25@mail2.sysu.edu.cn>
2021-04-19 17:49:59 +0800

    Update extract_cp_ner_dp_results.py
    modify according to the new data format

--- ARM/data_analysis/extract_cp_ner_dp_results.py
+++ ARM/data_analysis/extract_cp_ner_dp_results.py
@@ -5,13 +5,12 @@ from tqdm import tqdm
 import re
 from tqdm import tqdm
 import sys
-sys.path.append('/home/v-wanzho/v-wanzho/LSAT/code')
 from rule_pattern import RulePattern
 from collections import Counter
 RP = RulePattern()
-basic_dir = '/home/v-wanzho/v-wanzho/LSAT/data'
-datap = os.path.join(basic_dir, 'original_data/AR_DevelopmentData.json')
-outp =  os.path.join(basic_dir, 'cp_ner_dp_results/AR_DevelopmentData_cp_ner_dp_results.json')
+basic_dir = "../../data/"
+datap = os.path.join(basic_dir, 'AR_DevelopmentData.json')
+outp = os.path.join(basic_dir, 'cp_ner_dp_results/AR_DevelopData_cp_ner_dp_results.json')
 data = json.load(open(datap, 'r'))
 def clean_doc(doc):
     # doc = ' '+doc
@@ -20,45 +19,45 @@ def clean_doc(doc):
     doc = doc.replace('\u2014',' \u2014 ')
     return doc
 all_passages = []
-for exam in tqdm(data):
-    passages = exam['sections'][0]['passages']
+for passage in tqdm(data):
+    # passages = exam['sections'][0]['passages']
 
-    for pidx,passage in enumerate(passages):
-        doc = clean_doc(passage['passage'])#incase the first word is a number
-        passages[pidx]['passage'] = doc
-        ques = passage['questions']
-        sens = nltk.sent_tokenize(doc)
+    # for pidx,passage in enumerate(passages):
+    doc = clean_doc(passage['passage'])#incase the first word is a number
+    passage['passage'] = doc
+    ques = passage['questions']
+    sens = nltk.sent_tokenize(doc)
 
-        sen_tokens, sen_ent_res, sen_dps = RP.get_cp_ner_results(sens)
-        passages[pidx]['sentences'] = sens
-        passages[pidx]['sentence_cp_results'] = []
-        passages[pidx]['sentence_ner_results'] = []
-        passages[pidx]['sentence_dp_results'] = []
-        for idx in range(len(sens)):
+    sen_tokens, sen_ent_res, sen_dps = RP.get_cp_ner_results(sens)
+    passage['sentences'] = sens
+    passage['sentence_cp_results'] = []
+    passage['sentence_ner_results'] = []
+    passage['sentence_dp_results'] = []
+    for idx in range(len(sens)):
 
-            passages[pidx]['sentence_cp_results'].append(sen_tokens[idx])#['hierplane_tree']['root'])
-            passages[pidx]['sentence_ner_results'].append(sen_ent_res[idx])
-            passages[pidx]['sentence_dp_results'].append(sen_dps)
+        passage['sentence_cp_results'].append(sen_tokens[idx])#['hierplane_tree']['root'])
+        passage['sentence_ner_results'].append(sen_ent_res[idx])
+        passage['sentence_dp_results'].append(sen_dps)
 
-        #analyze keywords in question and answer
-        ques_text = [que['question'] for que in ques]
-        ques_tokens, ques_ent_res, ques_dps = RP.get_cp_ner_results(ques_text)
-        all_ques_kws = []
-        all_opt_kws = []
-        for idx in range(len(ques)):
-            passages[pidx]['questions'][idx]['question_cp_results'] =  ques_tokens[idx]#['hierplane_tree']['root']
-            passages[pidx]['questions'][idx]['question_ner_results'] = ques_ent_res[idx]
-            passages[pidx]['questions'][idx]['question_dp_results'] = ques_dps[idx]
+    #analyze keywords in question and answer
+    ques_text = [que['question'] for que in ques]
+    ques_tokens, ques_ent_res, ques_dps = RP.get_cp_ner_results(ques_text)
+    all_ques_kws = []
+    all_opt_kws = []
+    for idx in range(len(ques)):
+        passage['questions'][idx]['question_cp_results'] =  ques_tokens[idx]#['hierplane_tree']['root']
+        passage['questions'][idx]['question_ner_results'] = ques_ent_res[idx]
+        passage['questions'][idx]['question_dp_results'] = ques_dps[idx]
 
-            options = ques[idx]['options']
-            # print(options)
-            opt_tokens, opt_ent_res, opt_dps = RP.get_cp_ner_results(options)
-            passages[pidx]['questions'][idx]['option_cp_results'] = opt_tokens#[token['hierplane_tree']['root'] for token in opt_tokens]
-            passages[pidx]['questions'][idx]['option_ner_results'] = opt_ent_res
-            passages[pidx]['questions'][idx]['option_dp_results'] = opt_dps
+        options = ques[idx]['options']
+        # print(options)
+        opt_tokens, opt_ent_res, opt_dps = RP.get_cp_ner_results(options)
+        passage['questions'][idx]['option_cp_results'] = opt_tokens#[token['hierplane_tree']['root'] for token in opt_tokens]
+        passage['questions'][idx]['option_ner_results'] = opt_ent_res
+        passage['questions'][idx]['option_dp_results'] = opt_dps
 
 
-    all_passages.extend(passages)
+    all_passages.append(passage)
 
 with open(outp,'w',encoding='utf8') as outf:
     json.dump(all_passages,outf)
